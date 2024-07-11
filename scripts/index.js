502a7268a81b1922965621c5f83e9dd123042518
Marina Elizarova <seagirl1110@gmail.com>
2024-07-11 12:07:57 +0300

    feat: add close sidebar-menu

--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,23 @@ const sidebar = document.querySelector('.sidebar');
 const openBtn = document.querySelector('.btn-menu--open');
 const closeBtn = document.querySelector('.btn-menu--close');
 
-openBtn.addEventListener('click', () => {
+sidebar.addEventListener('click', (evt) => {
+  evt.stopPropagation();
+});
+
+openBtn.addEventListener('click', (evt) => {
+  evt.stopPropagation();
   sidebar.style.left = 0;
 });
 
-closeBtn.addEventListener('click', () => {
+const closeMenu = () => {
   sidebar.style.left = '-340px';
+};
+
+closeBtn.addEventListener('click', closeMenu);
+window.addEventListener('click', closeMenu);
+window.addEventListener('keydown', (evt) => {
+  if (evt.code === 'Escape') {
+    closeMenu();
+  }
 });
